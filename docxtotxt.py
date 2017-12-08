300990df294511a294057e48c6aa007f97aaffa9
KameshMadhavan <32696231+KameshMadhavan@users.noreply.github.com>
2017-12-08 14:34:15 +0530

    Update docxtotxt.py

--- docxtotxt.py
+++ docxtotxt.py
@@ -7,13 +7,11 @@ import sys
 reload(sys)
 sys.setdefaultencoding('utf8')
 
-
+# insert the folder path in which the docx file contains
 folderpath="/home/liferayapp/Documents/index/data/Index/David-Springer"
-# folderpath="/home/liferayapp/Documents/index/data/Index/Mcdonald-JBL"
 
-
+# path where the text file to be written
 out= open('/home/liferayapp/Documents/index/data/Index/fulldavidfile.txt','w')
-
 
 filelist=[]
 pattern = re.compile('([0-9]*).docx')
@@ -21,17 +19,12 @@ pattern = re.compile('([0-9]*).docx')
 for files in os.listdir(folderpath):
 	if files.endswith('.docx'):
 		templist=[]
-		print files
 		number = pattern.search(files).group(1)
-		print number
 		templist.append(int(number))
 		templist.append(files)
 		filelist.append(templist)
 
-print filelist
-
 sortfilelist = sorted(filelist, key=itemgetter(0))
-print sortfilelist
 
 for i in sortfilelist:
 	inputpath = str(folderpath)+str('/')+str(i[1])
